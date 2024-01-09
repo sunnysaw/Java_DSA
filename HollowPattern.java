f8a79c9518b3dcdf6e54edd9392a3671acc135c4
sunnyroy8084401051@gmail.com <sunnyroy8084401051@gmail.com>
2024-01-09 19:17:31 +0530

    Initial commit

---
+++ HollowPattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class HollowPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if (i == 1 || i == rows || j == 1 || j == columns) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
